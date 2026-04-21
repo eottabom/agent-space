6b4ccb61696ff7aac9e747baf68dea55806bb93e
eottabom <snoopy12oyk@gmail.com>
2026-04-21 09:09:57 +0900

    Update Vite config to use Terser for minification

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,8 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    minifyIdentifiers: false
+  build: {
+    minify: 'terser',
   },
   resolve: {
     alias: {
